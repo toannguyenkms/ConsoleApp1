d00a61956986143948adb52028c65f91574314fb
toan-mdsol <tonguyen@mdsol.com>
2021-07-27 10:44:16 +0700

    Fix comment

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello KMS");
+            Console.WriteLine("Hello KMS456");
         }
     }
 }
